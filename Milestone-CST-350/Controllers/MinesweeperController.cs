abd7b13c3ead4c938dd0e8dec638265acdd9135f
Caden Silvey <caden@silveys.net>
2023-10-15 09:15:57 -0700

    Simple version of minesweeper
    when I click on a cell you are able to turn it to a bomb.png

---
+++ Milestone-CST-350/Controllers/MinesweeperController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Milestone_CST_350.Models;
+
+namespace Milestone_CST_350.Controllers
+{
+    public class MinesweeperController : Controller
+    {
+
+        // create the grid for the minesweeper board 
+        static List<MinesweeperModel> ms = new List<MinesweeperModel>();
+        Random random = new Random();
+        const int GRID_SIZE = 25;
+
+        public IActionResult Index()
+        {
+           // empty the list when the page loads
+           ms = new List<MinesweeperModel>();
+
+            // Generate some new cells. 
+            for(int i = 0; i < GRID_SIZE; i++)
+            {
+                ms.Add(new MinesweeperModel(i, random.Next(4)));
+            }
+
+            // send the grid to the Index page
+            return View("Index", ms);
+        }
+
+
+        public IActionResult ToggleFlag(string buttonNumber)
+        {
+            // Convert from string to int
+            int bN = int.Parse(buttonNumber);
+
+            // Set the IsBomb property to true for the clicked cell
+            ms.ElementAt(bN).IsBomb = true;
+
+            // Re-display the buttons
+            return View("Index", ms);
+        }
+
+
+    }
+}
---
+++ Milestone-CST-350/Models/MinesweeperModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Milestone_CST_350.Models
+{
+    public class MinesweeperModel
+    {
+        public int Id { get; set; }
+        public int ButtonState { get; set; }
+        public bool IsBomb { get; set; }
+
+        public MinesweeperModel(int id, int buttonState)
+        {
+            Id = id;
+            ButtonState = buttonState;
+            IsBomb = false;
+        }
+
+
+
+
+
+        // implement in the next milestone
+
+        //public Cell[,] Cells { get; set; }
+        //public GameState GameStatus { get; set; }
+
+
+    }
+}
